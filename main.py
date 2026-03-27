7b5bb4560b41d33b27679f79378150a0759bc588
nerdpremier <touchpeera@gmail.com>
2026-03-27 13:32:00 +0700

    engine

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ MODEL_PATH = os.getenv("MODEL_PATH", "isolation_forest_model.pkl")
 API_SECRET = os.getenv("RISK_API_SECRET", "change-me")
 
 PIVOT_RATIO = float(os.getenv("PIVOT_RATIO", "0.10"))
-ANOMALY_GAMMA = float(os.getenv("ANOMALY_GAMMA", "2.0"))
+ANOMALY_GAMMA = float(os.getenv("ANOMALY_GAMMA", "2.5"))
 
 EPS = 1e-12
 
